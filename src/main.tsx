a0489e4eb475b99819ee96641db6a78648fbf002
Brian Julius Kiss <julius.brian.kiss@student.tuke.sk>
2023-09-18 23:07:32 +0200

    formatting

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./css/header.css";
 import "./css/global.css";
 import AllPages from "./components/AllPages.tsx";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AllPages></AllPages>
